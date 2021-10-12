c54f603140456031c9300ac5ce86339f9f25f038
Parv Garg <parv3213@gmail.com>
2021-10-12 20:15:55 +0530

    working self id with react CRA 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { EthereumAuthProvider, SelfID, WebClient } from '@self.id/web'
+
+const connectSelfId = async () => {
+	// The following assumes there is an injected `window.ethereum` provider
+	// @ts-ignore
+	const addresses = await window.ethereum.enable()
+	// @ts-ignore
+	const authProvider = new EthereumAuthProvider(window.ethereum, addresses[0])
+
+	// The following configuration assumes your local node is connected to the Clay testnet
+	const client = new WebClient({
+		ceramic: 'testnet-clay',
+		connectNetwork: 'testnet-clay',
+	})
+
+	// If authentication is successful, a DID instance is returned
+	const did = await client.authenticate(authProvider)
+
+	// A SelfID instance can only be created with an authenticated DID
+	const self = new SelfID({ client, did })
+	console.log('did', did)
+
+	console.log(await self.get('basicProfile'))
+}
 
 function App() {
   return (
@@ -18,6 +42,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={connectSelfId}>Click me</button>
       </header>
     </div>
   );
